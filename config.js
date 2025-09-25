f802f69cee90fa8dc236b21415955654b7cc2c2a
DominicDev <s0l0estudi4r@gmail.com>
2025-09-25 20:11:40 +0200

    v0.0.2

---
+++ config.js
@@ -0,0 +1,23 @@
+export const nameBand = 'HOLLOW COVES';
+export const nameSongs = [
+	"THESE MEMORIES", /*1*/
+	"THE WOODS", /*2*/
+	"COASTLINE", /*3*/
+	"BLESSINGS", /*4*/
+	"THE OPEN ROAD", /*5*/
+	"ANEW", /*6*/
+	"WHEN WE WERE YOUNG", /*7*/
+	"ON THE WAY", /*8*/
+	"MOMENTS", /*9*/
+	"RAN AWAY", /*10*/
+	"BORDERLINES", /*11*/
+	"PURPLE", /*12*/
+	"EVERMORE", /*13*/
+	"FROM THE SECOND FLOOR", /*14*/
+	"HOME", /*15*/
+	"LETTING GO", /*16*/
+	"LONELY NIGHTS", /*17*/
+	"SEE YOU SOON", /*18*/
+	"MILK & HONEY", /*19*/
+	"FACT OR FICTION", /*20*/
+]
